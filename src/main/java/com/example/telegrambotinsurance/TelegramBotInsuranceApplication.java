e62978f4f1bcac89f888b0623045b1d570903c97
babinvas <babinvas@gmail.com>
2020-10-12 21:21:01 +0300

    Тест.

--- src/main/java/com/example/telegrambotinsurance/TelegramBotInsuranceApplication.java
+++ src/main/java/com/example/telegrambotinsurance/TelegramBotInsuranceApplication.java
@@ -10,5 +10,9 @@ public class TelegramBotInsuranceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotInsuranceApplication.class, args);
 	}
-	
+	/*
+	 Ну, с почином.
+	 */
+
+	// Вторя попытка
 }
