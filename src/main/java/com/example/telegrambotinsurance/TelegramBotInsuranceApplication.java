fac573adea5b84ae40a38c7034266973c2262206
Stas Pasynkov <s.pasynkov@gmail.com>
2020-10-12 18:57:16 +0300

    Fixed error that data source is not defined. Temporarily excluded scanning.

--- src/main/java/com/example/telegrambotinsurance/TelegramBotInsuranceApplication.java
+++ src/main/java/com/example/telegrambotinsurance/TelegramBotInsuranceApplication.java
@@ -2,8 +2,9 @@ package com.example.telegrambotinsurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TelegramBotInsuranceApplication {
 
 	public static void main(String[] args) {
