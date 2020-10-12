7fb654adcb67832257b1064389fa380ef93c9c5f
Stas Pasynkov <s.pasynkov@gmail.com>
2020-10-12 18:45:16 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrambotinsurance/TelegramBotInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotInsuranceApplication.class, args);
+	}
+
+}
