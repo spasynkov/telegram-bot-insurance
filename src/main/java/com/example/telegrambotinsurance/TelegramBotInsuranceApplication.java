6f17421551fadf82690529eb61fdf371e1570c09
babinvas <babinvas@gmail.com>
2020-10-14 22:27:48 +0300

    Create greetings page #2 - Welcome page 1. Created a package in the package - java: controller. 2. Created a class in the package - controller: MainController. 3. Created a method in the class MainController: htmlWelcome. 4. Created a html file in the package - resources: welcome. 5. Filled the file - welcome with a simple Welcome content.

--- src/main/java/com/example/telegrambotinsurance/TelegramBotInsuranceApplication.java
+++ src/main/java/com/example/telegrambotinsurance/TelegramBotInsuranceApplication.java
@@ -10,8 +10,4 @@ public class TelegramBotInsuranceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotInsuranceApplication.class, args);
 	}
-
-	/*
-	 Ну, с почином.
-	 */
 }
---
+++ src/main/java/com/example/telegrambotinsurance/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.telegrambotinsurance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+// Класс главный контроллер, отвечает за обработку запросов на главную страницу ("/").
+public class MainController {
+
+    // Обрабатывать запросы, которые приходят на адрес "/" (на главную страницу).
+    // Возвращает название html-ки, которую спринг покажет пользователю в ответ.
+    @RequestMapping("/")
+    public String htmlWelcome() {
+        return "src/main/resources/static/welcome.html";
+    }
+}
