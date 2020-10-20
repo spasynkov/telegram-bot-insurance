11c77042e7d71dc5d4febc6fc980b89c9611e48e
klimenkoMA <klimenko.ma.1982@gmail.com>
2020-10-20 19:06:18 +0300

    #3 Some problems on RestController with return Set[]

---
+++ src/test/java/com/example/telegrambotinsurance/TGBotRestControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.telegrambotinsurance;
+
+import com.example.telegrambotinsurance.controller.TGBotRestController;
+import org.junit.jupiter.api.Test;
+
+public class TGBotRestControllerTest extends TGBotRestController {
+
+//    @Test
+//    public Cat restRequestShouldReturnHello(){
+//
+//
+//    }
+
+
+}
