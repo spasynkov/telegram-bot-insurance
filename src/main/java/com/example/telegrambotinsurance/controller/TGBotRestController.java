b4c40ca4bb777eb0924f230dbc468af4d225c037
klimenkoMA <klimenko.ma.1982@gmail.com>
2020-10-21 14:36:37 +0300

    #3 Complete build RestController and write test for rest() method

--- src/main/java/com/example/telegrambotinsurance/controller/TGBotRestController.java
+++ src/main/java/com/example/telegrambotinsurance/controller/TGBotRestController.java
@@ -2,33 +2,17 @@ package com.example.telegrambotinsurance.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 
 @RestController
 public class TGBotRestController {
 
     @RequestMapping("/rest")
-    @ResponseBody
-    public Date rest(){
-        return new Date();
-//    public List<Dog> rest() {
-//    public Dog[] rest() {
-//        Dog dog = new Dog();
-//        dog.dogSetFactory();
-//        Dog[] set = dog.dogSet;
-//        return set;
-//        dog.dogFactory();
-//        List<Dog> list = dog.dogList;
-//        return list;
+    public Cat rest() {
+        return new Cat();
     }
 
-    public class Cat {
+     public class Cat {
         private String message = "Hello";
 
         public String getMessage() {
@@ -39,75 +23,6 @@ public class TGBotRestController {
             this.message = message;
         }
 
-        @Override
-        public String toString() {
-            return "Cat{" +
-                    "message='" + message + '\'' +
-                    '}';
-        }
     }
 
-    public class Dog {
-        private int age = 5;
-        private String name = "WaPuK";
-
-        private List<Dog> dogList = new ArrayList<>();
-        private Dog[] dogSet = new Dog[age];
-
-        public Dog[] getDogSet() {
-            return dogSet;
-        }
-
-        public void setDogSet(Dog[] dogSet) {
-            this.dogSet = dogSet;
-        }
-
-        public void dogSetFactory() {
-            for (int i = 0; i < dogSet.length; i++) {
-                dogSet[i] = new Dog();
-                dogSet[i].setName(i + "Gav" + i);
-                dogSet[i].setAge(i);
-            }
-        }
-
-        public void dogFactory() {
-            for (int i = 0; i < age; i++) {
-                dogList.add(new Dog());
-                dogList.get(i).setName("RrR" + i);
-                dogList.get(i).setAge(i);
-            }
-        }
-
-        public List<Dog> getDogList() {
-            return dogList;
-        }
-
-        public void setDogList(List<Dog> dogList) {
-            this.dogList = dogList;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return "Dog{" +
-                    "age=" + age +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
-    }
 }
--- src/test/java/com/example/telegrambotinsurance/TGBotRestControllerTest.java
+++ src/test/java/com/example/telegrambotinsurance/TGBotRestControllerTest.java
@@ -1,15 +1,20 @@
 package com.example.telegrambotinsurance;
 
 import com.example.telegrambotinsurance.controller.TGBotRestController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TGBotRestControllerTest extends TGBotRestController {
+public class TGBotRestControllerTest {
 
-//    @Test
-//    public Cat restRequestShouldReturnHello(){
-//
-//
-//    }
+    @Test
+    public void newReturnRestShouldBeMessageHello() {
+        //action
+        TGBotRestController tgBotRestController = new TGBotRestController();
+        String message = tgBotRestController.rest().getMessage();
+        String expectAnswer = "Hello";
 
+        //assertion
+        Assertions.assertEquals(message, expectAnswer);
 
+    }
 }
