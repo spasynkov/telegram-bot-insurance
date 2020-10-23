0b8b02d591c93c590d6d7c9fd4acb3a088d280a2
babinvas <babinvas@gmail.com>
2020-10-23 21:37:01 +0300

    Task#4 Changed the Message class, added RequestMapping to the class and a method that handles the request for "/rest".

--- src/main/java/com/example/telegrambotinsurance/controller/RController.java
+++ src/main/java/com/example/telegrambotinsurance/controller/RController.java
@@ -3,60 +3,38 @@ package com.example.telegrambotinsurance.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+/**
+
+ * Класс ливит запросы на корневой запрос по адресу "/rest"
+
+ */
+
 
 @RestController
+@RequestMapping("/rest")
 public class RController {
 
     public RController() {
     }
 
-    @RequestMapping("/rest")
+    // Метод ливит запросы на корневой запрос по адресу "/rest".
+    @RequestMapping()
     public Message getMessage() {
         return new Message();
     }
 
     private class Message {
-        private String userName = "Вася";
-        private int userAge = 30;
-        private List<String> userMessages;
+        private String message = "Hello";
 
         public Message() {
-            userMessages = new ArrayList<>();
-            userMessages.add("Привет!");
-            userMessages.add("Это мы Боты!");
-            userMessages.add("Тебе конец кожаный мешок!");
         }
 
-        public Message(String userName, int age, List<String> messages) {
-            this.userName = userName;
-            this.userAge = age;
-            this.userMessages = messages;
+        public Message(String message) {
+            this.message = message;
         }
 
-        public String getName() {
-            return userName;
-        }
-
-        public void setName(String name) {
-            this.userName = name;
-        }
-
-        public int getAge() {
-            return userAge;
-        }
-
-        public void setAge(int age) {
-            this.userAge = age;
-        }
-
-        public List<String> getMessages() {
-            return userMessages;
-        }
-
-        public void setMessages(List<String> messages) {
-            this.userMessages = messages;
+        public String getMessage() {
+            return message;
         }
     }
 }
