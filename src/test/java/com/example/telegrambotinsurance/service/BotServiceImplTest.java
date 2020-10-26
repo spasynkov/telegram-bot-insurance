ea42e2eca3ac55e4619a33d1792f318463443671
MrErgos <66214678+MrErgos@users.noreply.github.com>
2020-10-27 00:00:21 +0300

    #8 Delete ServiceTest and Create BotServiceImplTest

--- src/test/java/com/example/telegrambotinsurance/service/BotServiceImplTest.java
+++ src/test/java/com/example/telegrambotinsurance/service/BotServiceImplTest.java
@@ -1,12 +1,9 @@
-package com.example.telegrambotinsurance;
+package com.example.telegrambotinsurance.service;
 
 import com.example.telegrambotinsurance.exception.BotNotFoundException;
 import com.example.telegrambotinsurance.modelbot.AbstractBot;
-import com.example.telegrambotinsurance.service.BotServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,8 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Map;
 
 @SpringBootTest
-class ServiceTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BotServiceImpl.class);
+class BotServiceImplTest {
     //Здесь Spring вносит значения в нужные поля
     //Аннотация @Qualifier нужна, так как без неё, Spring создает свою Map
     @Autowired
@@ -25,17 +21,16 @@ class ServiceTest {
     @Autowired
     BotServiceImpl botService;
 
-    //В этом тесте сервис находит бота по токену и проверяет один ли это и тот же объект
+    //В этом тесте сервис находит бота по токену и проверяет один ли это и тот же объект findBotByToken()
     @Test
-    void serviceTest(){
+    void findBotByToken() {
         try {
             String token = (String) bots.keySet().toArray()[0];
             AbstractBot bot = bots.get(token);
             AbstractBot bot1 = botService.findBotByToken(token);
             Assertions.assertSame(bot,bot1);
         } catch (BotNotFoundException e) {
-            LOGGER.debug(e.toString());
-            Assertions.fail();
+            Assertions.fail("Bot not found", e);
         }
 
     }
