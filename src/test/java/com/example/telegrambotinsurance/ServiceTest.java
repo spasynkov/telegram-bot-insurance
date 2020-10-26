9dd40fc158e2d20ca86ef7f1445592c6465d4ec5
MrErgos <66214678+MrErgos@users.noreply.github.com>
2020-10-26 19:38:58 +0300

    #8 Fixed ServiceTest

--- src/test/java/com/example/telegrambotinsurance/ServiceTest.java
+++ src/test/java/com/example/telegrambotinsurance/ServiceTest.java
@@ -5,6 +5,8 @@ import com.example.telegrambotinsurance.modelbot.AbstractBot;
 import com.example.telegrambotinsurance.service.BotServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +15,7 @@ import java.util.Map;
 
 @SpringBootTest
 class ServiceTest {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(BotServiceImpl.class);
     //Здесь Spring вносит значения в нужные поля
     //Аннотация @Qualifier нужна, так как без неё, Spring создает свою Map
     @Autowired
@@ -32,7 +34,8 @@ class ServiceTest {
             AbstractBot bot1 = botService.findBotByToken(token);
             Assertions.assertSame(bot,bot1);
         } catch (BotNotFoundException e) {
-            e.printStackTrace();
+            LOGGER.debug(e.toString());
+            Assertions.fail();
         }
 
     }
