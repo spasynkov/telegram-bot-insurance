ed5feb823f5e52c8e643dd65f7ef3f371f2da234
babinvas <babinvas@gmail.com>
2020-10-29 18:56:49 +0300

    #9 - Created a interface (HandlerWebHoodRequest) in the service package.

--- src/main/java/com/example/telegrambotinsurance/controller/BotRestController.java
+++ src/main/java/com/example/telegrambotinsurance/controller/BotRestController.java
@@ -1,5 +1,6 @@
 package com.example.telegrambotinsurance.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
