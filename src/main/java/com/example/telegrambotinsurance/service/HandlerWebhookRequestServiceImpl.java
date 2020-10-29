ea172d259d1d8f018516a9beee010b8275b65d87
babinvas <babinvas@gmail.com>
2020-10-29 19:43:14 +0300

    #9 Renamed class, interface and method from the wrong WebHood to the right Webhook

--- src/main/java/com/example/telegrambotinsurance/service/HandlerWebhookRequestServiceImpl.java
+++ src/main/java/com/example/telegrambotinsurance/service/HandlerWebhookRequestServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.telegrambotinsurance.service;
 
 import com.example.telegrambotinsurance.modelbot.Message;
+import org.springframework.stereotype.Service;
 
-public class HandlerWebHoodRequestServiceImpl implements HandlerWebHoodRequestService {
+@Service
+public class HandlerWebhookRequestServiceImpl implements HandlerWebhookRequestService {
 	public void processMessage(Message message) {
 
 	}
