b62d5c2de60c66d20d1bea51e333614691eda848
babinvas <babinvas@gmail.com>
2020-10-31 21:25:54 +0300

    #9 Renamed some classes, interface.

--- src/main/java/com/example/telegrambotinsurance/service/ClientService.java
+++ src/main/java/com/example/telegrambotinsurance/service/ClientService.java
@@ -2,6 +2,5 @@ package com.example.telegrambotinsurance.service;
 
 import com.example.telegrambotinsurance.modelbot.Message;
 
-public interface HandlerWebhookRequestService {
-	void processMessage(Message message);
+public interface ClientService {
 }
---
+++ src/main/java/com/example/telegrambotinsurance/service/ClientServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.telegrambotinsurance.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+}
