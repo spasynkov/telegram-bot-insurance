9345f9f5628501ad4eeec10510fea3f3f9f17a46
babinvas <babinvas@gmail.com>
2020-11-01 13:36:29 +0300

    #9 - Created the ClientService clientService field and implemented the getClientPostRequest method in the ClientController class. - Created the JSONObject jsonObject and String token fields, created the createMessage method in the ClientServiceImpl class.

--- src/main/java/com/example/telegrambotinsurance/service/ClientService.java
+++ src/main/java/com/example/telegrambotinsurance/service/ClientService.java
@@ -1,6 +1,9 @@
 package com.example.telegrambotinsurance.service;
 
-import com.example.telegrambotinsurance.modelbot.Message;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ClientService {
+	void createMessage(String token, JSONObject jsonObject);
 }
--- src/main/java/com/example/telegrambotinsurance/service/ClientServiceImpl.java
+++ src/main/java/com/example/telegrambotinsurance/service/ClientServiceImpl.java
@@ -1,8 +1,16 @@
 package com.example.telegrambotinsurance.service;
 
+import com.example.telegrambotinsurance.modelbot.AbstractBot;
+import com.example.telegrambotinsurance.modelbot.Message;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientServiceImpl implements ClientService {
+	private JSONObject jsonObject;
+	private  String token;
 
+	public void createMessage(String token, JSONObject jsonObject) {
+
+	}
 }
