6bd769d455fc66dfb27b181d85bf1fdd8122c352
babinvas <babinvas@gmail.com>
2020-11-01 13:36:29 +0300

    #9 - Created the ClientService clientService field and implemented the getClientPostRequest method in the ClientController class. - Created the JSONObject jsonObject and String token fields, created the createMessage method in the ClientServiceImpl class.

--- src/main/java/com/example/telegrambotinsurance/controller/ClientController.java
+++ src/main/java/com/example/telegrambotinsurance/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.example.telegrambotinsurance.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.telegrambotinsurance.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,12 +11,15 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ClientController {
+	private ClientService clientService;
 
+	@Autowired
+	ClientController(ClientService clientService) {
+		this.clientService = clientService;
+	}
 
 	@PostMapping("/rest/{token}")
-	@ResponseBody
-	public void getWebhookPostRequest(@PathVariable("token") String token, JSONObject jsonObject) {
-		JSONObject d;
-		ObjectMapper dd;
+	public void getClientPostRequest(@PathVariable("token") String token, JSONObject jsonObject) {
+		clientService.createMessage(token, jsonObject);
 	}
 }
