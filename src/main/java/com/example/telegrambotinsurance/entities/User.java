66f89e54c9a20472c4d4335af05018bfe7b7ece8
klimenkoMA <klimenko.ma.1982@gmail.com>
2020-11-03 07:55:53 +0300

    #3 rebuilding  tests, User class - deleted import *.*.List

--- src/main/java/com/example/telegrambotinsurance/entities/User.java
+++ src/main/java/com/example/telegrambotinsurance/entities/User.java
@@ -1,10 +1,11 @@
 package com.example.telegrambotinsurance.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-
-import lombok.*;
-
-import java.util.List;
 
 @Data
 @EqualsAndHashCode(exclude = "id")
@@ -14,15 +15,15 @@ import java.util.List;
 @Table(name = "users")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
 
-    // create new user without id
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
+	// create new user without id
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
 }
