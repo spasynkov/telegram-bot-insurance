14b735ebf58a084f66dcfc849bb3696e4c3f4355
Stas Pasynkov <s.pasynkov@gmail.com>
2020-11-06 02:25:39 +0200

    #13 Created SendRequestsService with empty methods

---
+++ src/main/java/com/example/telegrambotinsurance/service/SendRequestsService.java
@@ -0,0 +1,14 @@
+package com.example.telegrambotinsurance.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendRequestsService {
+	public Object sendGet(String token, String apiMessageName) {
+		return null;
+	}
+
+	public Object sendPost(String token, String apiMessageName, String json) {
+		return null;
+	}
+}
