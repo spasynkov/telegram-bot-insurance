74838d750c2401add598a885899eff70c262a829
klimenkoMA <klimenko.ma.1982@gmail.com>
2020-11-06 21:38:15 +0300

    #14 changes in HighLevelSendRequestService class - created 2 overloading sendMessage() methods

--- src/main/java/com/example/telegrambotinsurance/service/HighLevelSendRequestService.java
+++ src/main/java/com/example/telegrambotinsurance/service/HighLevelSendRequestService.java
@@ -1,8 +1,17 @@
 package com.example.telegrambotinsurance.service;
 
+import com.example.telegrambotinsurance.modelbot.AbstractBot;
+import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HighLevelSendRequestService {
 
+	public JSONObject sendMessage(AbstractBot bot, String token, Integer chatId, String message ){
+		return new JSONObject();
+	}
+
+	public JSONObject sendMessage(AbstractBot bot, String token, Integer chatId, String message, JSONObject json){
+		return new JSONObject();
+	}
 }
