356c34b49c2fc314b5c4f25422fce628c39ec5db
babinvas <babinvas@gmail.com>
2020-11-08 14:51:16 +0300

    fixup! #9 - Delete the created files conflicts

--- src/main/java/com/example/telegrambotinsurance/service/ClientService.java
+++ src/main/java/com/example/telegrambotinsurance/service/ClientService.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
 /**
  * Класс обработки все запросов от клиента с телеграм (POST, GET, PUT, DELETE).
  */
@@ -13,11 +12,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ClientService {
 	// Метод получает бота по токену, преобразует JSONObject в объект Message
-=======
-@Service
-public interface ClientService {
-	// Метод получает бота оп токену, преобразует  JSONObject в объект Message
->>>>>>> origin/Tas#9_Create-app-entry-point_BabinVas
 	// и предаёт этот объект бот через метод processMessage
 	void createMessage(String token, JSONObject jsonObject) throws BotNotFoundException, JsonProcessingException;
 }
