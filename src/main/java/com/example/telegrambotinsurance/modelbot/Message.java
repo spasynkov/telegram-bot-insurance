83eccd549eb2f2cdedc508c791198f76b295d112
babinvas <babinvas@gmail.com>
2020-11-11 17:55:46 +0300

    #9 - Added some fields in the Message class.

--- src/main/java/com/example/telegrambotinsurance/modelbot/Message.java
+++ src/main/java/com/example/telegrambotinsurance/modelbot/Message.java
@@ -1,5 +1,47 @@
 package com.example.telegrambotinsurance.modelbot;
 
-public class Message {
+import java.util.Date;
 
+public class Message {
+	private long message_id;
+	private Date date;
+	private String text;
+
+	public Message() {
+
+	}
+
+	public Message(long message_id, Date date, String text) {
+		this.message_id = message_id;
+		this.date = date;
+		this.text = text;
+	}
+
+	public long getMessage_id() {
+		return message_id;
+	}
+
+	public void setMessage_id(long message_id) {
+		this.message_id = message_id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String toString() {
+		return "{"+ message_id + " - " + date + " - " + text + "}";
+	}
 }
