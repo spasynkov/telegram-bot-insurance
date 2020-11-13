3fe0bdd46493fe0d984b5ac6b104b89aa2429ab4
babinvas <babinvas@gmail.com>
2020-11-13 22:06:09 +0300

    #9 - Implemented the Lombok in the Message class.

--- src/main/java/com/example/telegrambotinsurance/modelbot/Message.java
+++ src/main/java/com/example/telegrambotinsurance/modelbot/Message.java
@@ -1,47 +1,16 @@
 package com.example.telegrambotinsurance.modelbot;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
 	private long message_id;
 	private Date date;
 	private String text;
-
-	public Message() {
-
-	}
-
-	public Message(long message_id, Date date, String text) {
-		this.message_id = message_id;
-		this.date = date;
-		this.text = text;
-	}
-
-	public long getMessage_id() {
-		return message_id;
-	}
-
-	public void setMessage_id(long message_id) {
-		this.message_id = message_id;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public String toString() {
-		return "{message_id: "+ message_id + ", date: " + date + ", text: " + text + "}";
-	}
 }
