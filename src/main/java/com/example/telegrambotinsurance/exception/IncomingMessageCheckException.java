d9b6377daee627db9e414ee2a72ba55b70e13fba
babinvas <babinvas@gmail.com>
2020-11-17 19:00:49 +0300

    #9 - A new exception has been created.

---
+++ src/main/java/com/example/telegrambotinsurance/exception/IncomingMessageCheckException.java
@@ -0,0 +1,10 @@
+package com.example.telegrambotinsurance.exception;
+
+public class IncomingMessageCheckException extends Exception {
+	public IncomingMessageCheckException() {
+	}
+
+	public IncomingMessageCheckException(String message) {
+		super(message);
+	}
+}
