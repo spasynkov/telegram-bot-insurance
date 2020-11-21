947132fc05ea6e4c877184e5a4508843fb6d19fd
klimenkoMA <klimenko.ma.1982@gmail.com>
2020-11-21 19:24:11 +0300

    #14 changes in HighLevelSendRequestService class - created enum TGApiRequest - deleted final String GET_ME

--- src/main/java/com/example/telegrambotinsurance/service/HighLevelSendRequestService.java
+++ src/main/java/com/example/telegrambotinsurance/service/HighLevelSendRequestService.java
@@ -1,5 +1,6 @@
 package com.example.telegrambotinsurance.service;
 
+
 import com.example.telegrambotinsurance.modelbot.AbstractBot;
 import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
@@ -8,41 +9,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-
 @Service
 public class HighLevelSendRequestService {
 
-	private final String GET_ME = "getMe";
-	SendRequestsService sendRequestsService;
 	private final Logger LOGGER = LoggerFactory.getLogger(HighLevelSendRequestService.class);
+	SendRequestsService sendRequestsService;
 
 	@Autowired
 	public HighLevelSendRequestService(SendRequestsService sendRequestsService) {
 		this.sendRequestsService = sendRequestsService;
 	}
 
-	public JSONObject sendMessage(AbstractBot bot, String token, Integer chatId, String message ){
+	public JSONObject sendMessage(AbstractBot bot, String token, Integer chatId, String message) {
 		return new JSONObject();
 	}
 
-	public JSONObject sendMessage(AbstractBot bot, String token, Integer chatId, String message, JSONObject json){
+	public JSONObject sendMessage(AbstractBot bot, String token, Integer chatId, String message, JSONObject json) {
 		return new JSONObject();
 	}
 
-	public String getChat(Integer chatId){
+	public String getChat(Integer chatId) {
 		String chatName = "Some name";
 		return chatName;
 	}
 
-	public Integer getChat(String chatName){
+	public Integer getChat(String chatName) {
 		Integer chatId = 0;
 		return chatId;
 	}
 
-	public JSONObject getMe(AbstractBot bot, String token){
-		JSONObject jsonObject = sendRequestsService.sendGet(token, GET_ME);
-		LOGGER.debug(GET_ME);
+	public JSONObject getMe(AbstractBot bot, String token) {
+		JSONObject jsonObject = sendRequestsService.sendGet(token, TGApiRequest.GET_ME.getRequest());
+		LOGGER.debug(TGApiRequest.GET_ME.getRequest());
 
 		return jsonObject;
+	}
+
+	public enum TGApiRequest {
+		GET_ME("getMe"), SEND_MESSAGE("sendMessage"), GET_CHAT("getChat");
+
+		private String request;
+
+		TGApiRequest(String request) {
+			this.request = request;
+		}
+
+		public String getRequest() {
+			return request;
+		}
 	}
 }
