2b2df59326ed8caf9c9d9717479f36ef3217013c
MrErgos <66214678+MrErgos@users.noreply.github.com>
2020-11-22 18:02:10 +0300

    #21 Add default constructor

--- src/main/java/com/example/telegrambotinsurance/exception/ButtonFormatException.java
+++ src/main/java/com/example/telegrambotinsurance/exception/ButtonFormatException.java
@@ -2,6 +2,9 @@ package com.example.telegrambotinsurance.exception;
 
 public class ButtonFormatException extends RuntimeException{
 
+	public ButtonFormatException() {
+	}
+
 	public ButtonFormatException(String message) {
 		super(message);
 	}
