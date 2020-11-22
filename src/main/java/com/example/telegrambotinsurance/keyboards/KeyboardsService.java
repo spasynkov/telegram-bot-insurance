db5381451c4735567018a75bdf5590a5d8913b2e
MrErgos <66214678+MrErgos@users.noreply.github.com>
2020-11-22 18:10:23 +0300

    #21 Changed KeyboardsService logic

--- src/main/java/com/example/telegrambotinsurance/keyboards/KeyboardsService.java
+++ src/main/java/com/example/telegrambotinsurance/keyboards/KeyboardsService.java
@@ -1,5 +1,6 @@
 package com.example.telegrambotinsurance.keyboards;
 
+import com.example.telegrambotinsurance.exception.ButtonFormatException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -7,94 +8,88 @@ import org.springframework.stereotype.Service;
 public class KeyboardsService {
 	private Keyboard keyboard;
 
-
 	/**
-	 * @return Возращает стартовое меню в JSON виде
+	 * Создание reply клавиатуры
 	 */
-	public JSONObject getStarterMenu(){
-		keyboard = new ReplyKeyboardMarkup(true,true);
-		keyboard.addKeyboardRow();
-		keyboard.addKeyboardRow();
-		keyboard.addButton(0,"Оформить договор");
-		keyboard.addButton(1,"Контакты");
-		keyboard.addButton(1,"Оплата");
-		keyboard.addButton(1,"Помощь");
-
-		return new JSONObject(keyboard);
+	public void createReplyKeyboard(){
+		keyboard = new ReplyKeyboardMarkup();
 	}
 
 	/**
-	 * @return Возращает меню самых частых вопросов в JSON виде
+	 * Создание inline клавиатуры
 	 */
-	public JSONObject getFrequentQuestions(){
-		keyboard = new ReplyKeyboardMarkup(true,true);
-		keyboard.addKeyboardRow();
-		keyboard.addKeyboardRow();
-		keyboard.addButton(0,"Частые вопросы");
-		keyboard.addButton(1,"1");
-		keyboard.addButton(1,"2");
-		keyboard.addButton(1,"3");
-
-		return new JSONObject(keyboard);
-	}
-
-	/**
-	 * @return Возращает меню оформления договора в JSON виде
-	 */
-	public JSONObject getExecutionOfTheContract(){
-		keyboard = new ReplyKeyboardMarkup(true,true);
-		keyboard.addKeyboardRow();
-		keyboard.addButton(0,"Получить код");
-		keyboard.addButton(0,"Ввести код");
-
-		return new JSONObject(keyboard);
-	}
-
-	/**
-	 * @return Возращает меню получения кода в JSON виде
-	 */
-	public JSONObject getTheCode(){
-		keyboard = new ReplyKeyboardMarkup(true,true);
-		keyboard.addKeyboardRow();
-		keyboard.addButton(0,"Личная рекомендация");
-		keyboard.addButton(0,"Реклама");
-
-		return new JSONObject(keyboard);
-	}
-
-	/**
-	 * @return Возращает меню отправки номера телефона в JSON виде
-	 */
-	public JSONObject getSendUserNumber(){
-		keyboard = new ReplyKeyboardMarkup(true,true);
-		keyboard.addKeyboardRow();
-		keyboard.addButton(0, new KeyboardButton("Отправить свой номер телефона", true));
-
-		return new JSONObject(keyboard);
-	}
-
-	/**
-	 * Возращает строку, которая удаляет клавиатуру
-	 * @return Возращает строку в JSON виде
-	 */
-	public String removeKeyboard(){
-		return "{\"chat_id\":-1001484722738, \"text\":\"Test\", \"reply_markup\":{\"remove_keyboard\":true}}";
-	}
-
-	/**
-	 * ЭТО НЕ ИЗ ТЗ, ПРОСТО ДЛЯ ТЕСТА
-	 * Возращает строку, которая добавляет встроенную клавиатуру
-	 * @return Возращает строку в JSON виде
-	 */
-	public JSONObject getTestInlineKeyboard(){
+	public void createInlineKeyboard(){
 		keyboard = new InlineKeyboardMarkup();
-		keyboard.addKeyboardRow();
-		keyboard.addKeyboardRow();
-		keyboard.addButton(0, InlineButtonInterface.builder()
-				.setText("1").setUrl("https://core.telegram.org/bots/api#inlinekeyboardbutton").build());
-		keyboard.addButton(1, InlineButtonInterface.builder()
-				.setText("2").setUrl("https://core.telegram.org/bots/api#inlinekeyboardbutton").build());
+	}
 
+	/**
+	 * Метод добавляет один ряд кнопок
+	 */
+	public void addRow(){
+		keyboard.addKeyboardRow();
+	}
+
+	/**
+	 * Метод добавляет одну кнопку с переданным текстом
+	 *  в ряд под переданным числом и в позицию под переданным числом
+	 *  (отсчет начинается от 0)
+	 *
+	 * @param positionInTheRow Позиция кнопки в ряду
+	 * @param rowPosition Позиция ряда в List
+	 * @param button Кнопка
+	 */
+	public void addButton(int positionInTheRow, int rowPosition,Button button){
+		keyboard.addButton(positionInTheRow,rowPosition,button);
+	}
+
+	/**
+	 * Метод добавляет переданную кнопку в последний ряд
+	 *  в позицию под переданным числом (отсчет начинается от 0)
+	 *
+	 * @param positionInTheRow Позиция кнопки в ряду
+	 * @param button Кнопка
+	 */
+	public void addButton(int positionInTheRow,Button button){
+		keyboard.addButton(positionInTheRow,button);
+	}
+
+	/**
+	 * Метод добавляет переданную кнопку в последний ряд
+	 *  в последнию позицию в ряде
+	 *
+	 * @param button Кнопка
+	 */
+	public void addButton(Button button) {
+		keyboard.addButton(button);
+	}
+
+	/**
+	 * Возращает клавиатуру в JSONObject виде
+	 *
+	 * @return JSONObject
+	 */
+	public JSONObject getKeyboard(){
 		return new JSONObject(keyboard);
+	}
+
+	/**
+	 * Возращает объект удаления клавиатуры
+	 *
+	 * @return JSONObject
+	 */
+	public JSONObject removeKeyboard(){
+		ReplyKeyboardRemove replyKeyboardRemove = new ReplyKeyboardRemove();
+		return new JSONObject(replyKeyboardRemove);
+	}
+
+	/**
+	 * Возращает объект удаления клавиатуры
+	 *
+	 * @param selective
+	 * @return JSONObject
+	 */
+	public JSONObject removeKeyboard(Boolean selective){
+		ReplyKeyboardRemove replyKeyboardRemove = new ReplyKeyboardRemove(selective);
+		return new JSONObject(replyKeyboardRemove);
 	}
 }
