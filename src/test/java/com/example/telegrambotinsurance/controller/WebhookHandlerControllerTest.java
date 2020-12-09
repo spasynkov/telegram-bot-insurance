24c9e5e9ad29b7e13b9783ee52e0827ebe90ba1f
babinvas <babinvas@gmail.com>
2020-12-09 21:28:29 +0300

    #20 - Created a WebhookHandlerControllerTest class and its empty method.

---
+++ src/test/java/com/example/telegrambotinsurance/controller/WebhookHandlerControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotinsurance.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookHandlerControllerTest {
+
+	@Test
+	void testReceiveAndProcessMessage() {
+
+	}
+}
