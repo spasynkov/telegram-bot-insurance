210ab9c7045ccc2c50df1e767db69533b16e40ca
babinvas <babinvas@gmail.com>
2020-12-09 21:23:33 +0300

    #20 - A method of checking for the standard JSON exception throwing has been implemented.

--- src/test/java/com/example/telegrambotinsurance/service/WebhookHandlerServiceImplTest.java
+++ src/test/java/com/example/telegrambotinsurance/service/WebhookHandlerServiceImplTest.java
@@ -129,6 +129,42 @@ class WebhookHandlerServiceImplTest {
 		assertEquals(expected, actual, "The OK status test failed.");
 	}
 
+	@Test
+	void testReceiveAndProcessMessage_ShouldThrowsStandardJsonException() {
+		String jsonString = "{" +
+				"\"update_id\": 641170930," +
+				"\"message\": {" +
+				"\"from\": {" +
+				"\"id\": 652216725," +
+				"\"is_bot\": false," +
+				"\"first_name\": \"Stas\"," +
+				"\"last_name\": \"Pasynkov\"," +
+				"\"username\": \"spasynkov\"," +
+				"\"language_code\": \"uk\"" +
+				"}," +
+				"\"chat\": {" +
+				"\"id\": -485469147," +
+				"\"title\":\"\\u0420\\u0430\\u0437\\u0440\\u0430\\u0431\\u043e\\u0442\\u043a\\u0430 \\u0442\\u0435\\u043b\\u0435\\u0433\\u0440\\u0430\\u043c-\\u0431\\u043e\\u0442\\u0430\"," +
+				"\"type\": \"group\"," +
+				"\"all_members_are_administrators\":false" +
+				"}," +
+				"\"date\": 1586690677," +
+				"\"text\": \"\\u0440\\u0430\\u0441\\u0448\\u0430\\u0442\\u0430\\u043b\\u0438\\u0441\\u044c \\u043a \\u0447\\u0435\\u0440\\u0442\\u0443 \\u043d\\u0435\\u0440\\u0432\\u044b\"" +
+				"}" +
+				"}";
+
+		Exception exception = assertThrows(JSONException.class, () -> {
+					final JSONObject source = new JSONObject(jsonString);
+					webhookHandlerService.receiveAndProcessMessage(token, source);
+				}
+		);
+		String actualMessage = exception.getMessage();
+
+		String expectedMessage = "JSONObject[\"message_id\"] not found.";
+
+		assertEquals(actualMessage, expectedMessage, "The test '" + expectedMessage + "' failed.");
+	}
+
 	@ParameterizedTest
 	@MethodSource("provideStringsForTest")
 	void testReceiveAndProcessMessage_ShouldThrowsMessageException(String json, String expectedMessage) {
