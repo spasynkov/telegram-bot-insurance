9c1b784dd89db3611b077db5a132efcb103c0eaa
babinvas <babinvas@gmail.com>
2020-12-09 21:15:12 +0300

    #20 - The provideStringsForTest() method was implemented.

--- src/test/java/com/example/telegrambotinsurance/service/WebhookHandlerServiceImplTest.java
+++ src/test/java/com/example/telegrambotinsurance/service/WebhookHandlerServiceImplTest.java
@@ -29,10 +29,64 @@ class WebhookHandlerServiceImplTest {
 				Arguments.of(null, "The incoming object cannot be null."),
 				Arguments.of("{}", "The 'incoming' object cannot be empty."),
 				Arguments.of("{" +
-						"\"update_id\": 641170930," +
-						"\"message\": {" +
-						"}" +
-						"}", "The 'message' object cannot be empty.")
+								"\"update_id\": 641170930," +
+								"}",
+						"There is no 'message' block in object."),
+				Arguments.of("{" +
+								"\"update_id\": 641170930," +
+								"\"message\": " + null +
+								"}",
+						"The 'message' object cannot be null."),
+				Arguments.of("{" +
+								"\"update_id\": 641170930," +
+								"\"message\": {}" +
+								"}",
+						"The 'message' object cannot be empty."),
+				Arguments.of("{" +
+								"\"update_id\": 641170930," +
+								"\"message\": {" +
+								"\"message_id\": 785," +
+								"\"from\": {" +
+								"\"id\": 652216725," +
+								"\"is_bot\": false," +
+								"\"first_name\": \"Stas\"," +
+								"\"last_name\": \"Pasynkov\"," +
+								"\"username\": \"spasynkov\"," +
+								"\"language_code\": \"uk\"" +
+								"}," +
+								"\"chat\": {" +
+								"\"id\": -485469147," +
+								"\"title\":\"\\u0420\\u0430\\u0437\\u0440\\u0430\\u0431\\u043e\\u0442\\u043a\\u0430 \\u0442\\u0435\\u043b\\u0435\\u0433\\u0440\\u0430\\u043c-\\u0431\\u043e\\u0442\\u0430\"," +
+								"\"type\": \"group\"," +
+								"\"all_members_are_administrators\":false" +
+								"}," +
+								"\"date\": 1586690677," +
+								"}" +
+								"}",
+						"There is no 'text' block in object."),
+				Arguments.of("{" +
+								"\"update_id\": 641170930," +
+								"\"message\": {" +
+								"\"message_id\": 785," +
+								"\"from\": {" +
+								"\"id\": 652216725," +
+								"\"is_bot\": false," +
+								"\"first_name\": \"Stas\"," +
+								"\"last_name\": \"Pasynkov\"," +
+								"\"username\": \"spasynkov\"," +
+								"\"language_code\": \"uk\"" +
+								"}," +
+								"\"chat\": {" +
+								"\"id\": -485469147," +
+								"\"title\":\"\\u0420\\u0430\\u0437\\u0440\\u0430\\u0431\\u043e\\u0442\\u043a\\u0430 \\u0442\\u0435\\u043b\\u0435\\u0433\\u0440\\u0430\\u043c-\\u0431\\u043e\\u0442\\u0430\"," +
+								"\"type\": \"group\"," +
+								"\"all_members_are_administrators\":false" +
+								"}," +
+								"\"date\": 1586690677," +
+								"\"text\": " + null +
+								"}" +
+								"}",
+						"The 'text' object cannot be null.")
 		);
 	}
 
