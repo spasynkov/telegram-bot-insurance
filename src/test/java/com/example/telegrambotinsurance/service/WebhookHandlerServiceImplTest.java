48ee475d76d84472ce5c1e468d969a1f0005a539
babinvas <babinvas@gmail.com>
2020-12-10 23:34:58 +0300

    #20 - The WebhookHandlerServiceImplTest class has been adjusted to get external parameters for tests.

--- src/test/java/com/example/telegrambotinsurance/service/WebhookHandlerServiceImplTest.java
+++ src/test/java/com/example/telegrambotinsurance/service/WebhookHandlerServiceImplTest.java
@@ -1,17 +1,15 @@
 package com.example.telegrambotinsurance.service;
 
 import com.example.telegrambotinsurance.exception.MessageValidationException;
+import com.example.telegrambotinsurance.parameters.TestMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,97 +22,9 @@ class WebhookHandlerServiceImplTest {
 	@Value("${bot.insurance.token}")
 	String token;
 
-	private static Stream<Arguments> provideStringsForTest() {
-		return Stream.of(
-				Arguments.of(null, "The incoming object cannot be null."),
-				Arguments.of("{}", "The 'incoming' object cannot be empty."),
-				Arguments.of("{" +
-								"\"update_id\": 641170930," +
-								"}",
-						"There is no 'message' block in object."),
-				Arguments.of("{" +
-								"\"update_id\": 641170930," +
-								"\"message\": " + null +
-								"}",
-						"The 'message' object cannot be null."),
-				Arguments.of("{" +
-								"\"update_id\": 641170930," +
-								"\"message\": {}" +
-								"}",
-						"The 'message' object cannot be empty."),
-				Arguments.of("{" +
-								"\"update_id\": 641170930," +
-								"\"message\": {" +
-								"\"message_id\": 785," +
-								"\"from\": {" +
-								"\"id\": 652216725," +
-								"\"is_bot\": false," +
-								"\"first_name\": \"Stas\"," +
-								"\"last_name\": \"Pasynkov\"," +
-								"\"username\": \"spasynkov\"," +
-								"\"language_code\": \"uk\"" +
-								"}," +
-								"\"chat\": {" +
-								"\"id\": -485469147," +
-								"\"title\":\"\\u0420\\u0430\\u0437\\u0440\\u0430\\u0431\\u043e\\u0442\\u043a\\u0430 \\u0442\\u0435\\u043b\\u0435\\u0433\\u0440\\u0430\\u043c-\\u0431\\u043e\\u0442\\u0430\"," +
-								"\"type\": \"group\"," +
-								"\"all_members_are_administrators\":false" +
-								"}," +
-								"\"date\": 1586690677," +
-								"}" +
-								"}",
-						"There is no 'text' block in object."),
-				Arguments.of("{" +
-								"\"update_id\": 641170930," +
-								"\"message\": {" +
-								"\"message_id\": 785," +
-								"\"from\": {" +
-								"\"id\": 652216725," +
-								"\"is_bot\": false," +
-								"\"first_name\": \"Stas\"," +
-								"\"last_name\": \"Pasynkov\"," +
-								"\"username\": \"spasynkov\"," +
-								"\"language_code\": \"uk\"" +
-								"}," +
-								"\"chat\": {" +
-								"\"id\": -485469147," +
-								"\"title\":\"\\u0420\\u0430\\u0437\\u0440\\u0430\\u0431\\u043e\\u0442\\u043a\\u0430 \\u0442\\u0435\\u043b\\u0435\\u0433\\u0440\\u0430\\u043c-\\u0431\\u043e\\u0442\\u0430\"," +
-								"\"type\": \"group\"," +
-								"\"all_members_are_administrators\":false" +
-								"}," +
-								"\"date\": 1586690677," +
-								"\"text\": " + null +
-								"}" +
-								"}",
-						"The 'text' object cannot be null.")
-		);
-	}
-
 	@Test
 	void testReceiveAndProcessMessage_ShouldReturnOkStatus() {
-		String jsonString = "{" +
-				"\"update_id\": 641170930," +
-				"\"message\": {" +
-				"\"message_id\": 785," +
-				"\"from\": {" +
-				"\"id\": 652216725," +
-				"\"is_bot\": false," +
-				"\"first_name\": \"Stas\"," +
-				"\"last_name\": \"Pasynkov\"," +
-				"\"username\": \"spasynkov\"," +
-				"\"language_code\": \"uk\"" +
-				"}," +
-				"\"chat\": {" +
-				"\"id\": -485469147," +
-				"\"title\":\"\\u0420\\u0430\\u0437\\u0440\\u0430\\u0431\\u043e\\u0442\\u043a\\u0430 \\u0442\\u0435\\u043b\\u0435\\u0433\\u0440\\u0430\\u043c-\\u0431\\u043e\\u0442\\u0430\"," +
-				"\"type\": \"group\"," +
-				"\"all_members_are_administrators\":false" +
-				"}," +
-				"\"date\": 1586690677," +
-				"\"text\": \"\\u0440\\u0430\\u0441\\u0448\\u0430\\u0442\\u0430\\u043b\\u0438\\u0441\\u044c \\u043a \\u0447\\u0435\\u0440\\u0442\\u0443 \\u043d\\u0435\\u0440\\u0432\\u044b\"" +
-				"}" +
-				"}";
-
+		String jsonString = TestMessage.OkTextJsonString;
 		JSONObject source = new JSONObject();
 
 		try {
@@ -131,27 +41,7 @@ class WebhookHandlerServiceImplTest {
 
 	@Test
 	void testReceiveAndProcessMessage_ShouldThrowsStandardJsonException() {
-		String jsonString = "{" +
-				"\"update_id\": 641170930," +
-				"\"message\": {" +
-				"\"from\": {" +
-				"\"id\": 652216725," +
-				"\"is_bot\": false," +
-				"\"first_name\": \"Stas\"," +
-				"\"last_name\": \"Pasynkov\"," +
-				"\"username\": \"spasynkov\"," +
-				"\"language_code\": \"uk\"" +
-				"}," +
-				"\"chat\": {" +
-				"\"id\": -485469147," +
-				"\"title\":\"\\u0420\\u0430\\u0437\\u0440\\u0430\\u0431\\u043e\\u0442\\u043a\\u0430 \\u0442\\u0435\\u043b\\u0435\\u0433\\u0440\\u0430\\u043c-\\u0431\\u043e\\u0442\\u0430\"," +
-				"\"type\": \"group\"," +
-				"\"all_members_are_administrators\":false" +
-				"}," +
-				"\"date\": 1586690677," +
-				"\"text\": \"\\u0440\\u0430\\u0441\\u0448\\u0430\\u0442\\u0430\\u043b\\u0438\\u0441\\u044c \\u043a \\u0447\\u0435\\u0440\\u0442\\u0443 \\u043d\\u0435\\u0440\\u0432\\u044b\"" +
-				"}" +
-				"}";
+		String jsonString = TestMessage.missingParameterJsonString;
 
 		Exception exception = assertThrows(JSONException.class, () -> {
 					final JSONObject source = new JSONObject(jsonString);
@@ -166,7 +56,7 @@ class WebhookHandlerServiceImplTest {
 	}
 
 	@ParameterizedTest
-	@MethodSource("provideStringsForTest")
+	@MethodSource("com.example.telegrambotinsurance.parameters.TestMessage#provideStringsForTest")
 	void testReceiveAndProcessMessage_ShouldThrowsMessageException(String json, String expectedMessage) {
 		JSONObject source = json == null ? null : new JSONObject(json);
 
