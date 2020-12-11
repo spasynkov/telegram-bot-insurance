182fce99a37cf116bf8abb6f23d451e3881282f8
MrErgos <66214678+MrErgos@users.noreply.github.com>
2020-12-11 20:05:16 +0300

    #21 InlineKeyboardMarkup: Delete @NoArgsConstructor

--- src/main/java/com/example/telegrambotinsurance/keyboards/InlineKeyboardMarkup.java
+++ src/main/java/com/example/telegrambotinsurance/keyboards/InlineKeyboardMarkup.java
@@ -1,5 +1,6 @@
 package com.example.telegrambotinsurance.keyboards;
 
+import lombok.NoArgsConstructor;
 import org.json.JSONPropertyName;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 /**
  * Клавиатура, которая прикрепляется к сообщению
  */
+@NoArgsConstructor
 public class InlineKeyboardMarkup extends AbstractKeyboard{
 
 	@JSONPropertyName("inline_keyboard")
